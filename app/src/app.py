24cb693a2b6264f2232860f511e2fb058f22944b
Ibukun Didier <adjitchedidier1@gmail.com>
2020-12-17 18:14:18 -0400

    atualizando

--- app/src/app.py
+++ app/src/app.py
@@ -2,6 +2,8 @@ import os
 import glob
 import re
 import time
+
+
 from flask import Flask, send_from_directory, request, flash, url_for, redirect, render_template, jsonify
 
 from subprocess import call
@@ -23,7 +25,7 @@ def upload_file():
   if request.method == 'POST':
     file = request.files['image']
     foldername = str(int(time.time()))
-    filename = foldername+".png"
+    filename = foldername+".jpg"
     file.save(os.path.join(app.config['UPLOAD'], filename))
     
     call('python create_data.py',shell = True)
@@ -37,9 +39,12 @@ def upload_file():
         predict.append(re.findall(r"[@]+[a-zA-Z]+",im)[0][1:])
     
     return jsonify({"Classificacao": predict[0]})
+    
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
 
 
 
